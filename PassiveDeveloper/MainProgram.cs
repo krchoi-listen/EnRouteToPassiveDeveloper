b3f6e3a1f6e2ab1da89188dee797a5f48147aa9a
krchoi-listen <45301931+krchoi-listen@users.noreply.github.com>
2021-12-29 17:02:56 -0500

    Adding Project Directory from command line

---
+++ PassiveDeveloper/MainProgram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PassiveDeveloper
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
